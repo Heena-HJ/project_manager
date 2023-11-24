3a16996d2bc577512dc00ba419e3c1a9f2d7af9b
Heena Jain <heenadollyjain14@gmail.com>
2023-11-24 16:03:32 +0530

    changes

--- server/server.js
+++ server/server.js
@@ -5,12 +5,18 @@ require('../database/db');
 const authRoutes = require('./routes/authRoutes');
 const taskRouter = require('./routes/taskRoutes');
 const morgan = require('morgan');
+const path = require('path');
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/task', taskRouter);
+
+app.use(express.static(path.join(__dirname, './client/dist')));
+app.get("*", function(req, res){
+	res.sendFile(path.join(__dirname, './client/dist/index.html'))
+});
 
 // localhost:4000/auth/register
 
